148c6979f4e78ff29686df86a8c4766b77fb8c4f
piasun <77275966+piasun@users.noreply.github.com>
2022-09-01 19:08:42 +0200

    import hotjar all pages

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const baseURL = "http://localhost/re-yourself/wp-json/wp/v2/posts";
+const blogContainer = document.querySelector(".blogposts");
+
+
+
+async function getBlogposts(url) {
+    const response = await fetch(url);
+	const blogposts = await response.json();
+
+    console.log(blogposts);
+
+
+    blogposts.forEach(function(blogpost){
+        blogContainer.innerHTML += `
+                <article class="blogpost_card">
+                    <a href="blogpostspecific.html?id=${blogpost.id}" class="blogpost">
+                        <h3>${blogpost.title.rendered}</h3>
+                        <p>${blogpost.excerpt.rendered}</p>
+                        <div class="cta"> Read more...</div>
+                    </a>
+                </article>`
+    
+        })
+}
+
+getBlogposts(baseURL);
