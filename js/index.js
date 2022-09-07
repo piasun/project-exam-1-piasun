3715f4aec7b82dc990cef2242ed844d93f721de7
piasun <77275966+piasun@users.noreply.github.com>
2022-09-07 19:53:09 +0200

    updated index js, html and css

--- js/index.js
+++ js/index.js
@@ -1,29 +1,71 @@
+//elements in the dom
+const slider = document.querySelector(".slider");
+const prevButton = document.querySelector(".previous");
+const nextButton = document.querySelector(".next");
+
+//call api
 const baseURL = "http://localhost/re-yourself/wp-json/wp/v2/posts?_embed";
-const blogContainer = document.querySelector(".blogposts");
 
 
-
-async function getBlogposts(url) {
+async function getPosts(url) {
     const response = await fetch(url);
 	const blogposts = await response.json();
 
     console.log(blogposts);
 
 
-    blogposts.forEach(function(blogpost){
-        blogContainer.innerHTML += `
-                <article class="blogpost_card">
-                    <a href="blogpostspecific.html?id=${blogpost.id}" class="blogpost">
-                        <h3>${blogpost.title.rendered}</h3>
-                        <img src="${blogpost._embedded['wp:featuredmedia'][0].source_url}" />
+    blogposts.forEach(function(post){
+        slider.innerHTML += `
+                <article class="card">
+                    <a href="blogpostspecific.html?id=${post.id}" class="blogpost">
+                        <h3>${post.title.rendered}</h3>
+                        <img src="${post._embedded['wp:featuredmedia'][0].source_url}" />
                     </a>
-                    <div>
-                        <span>#${blogpost._embedded['wp:term'][0][0].slug}</span>
-                        <span>#${blogpost._embedded['wp:term'][0][1].slug}</span>
+                    <div class="category">
+                        <span>#${post._embedded['wp:term'][0][0].slug}</span>
+                        <span>#${post._embedded['wp:term'][0][1].slug}</span>
                     </div>
                 </article>`
     
         })
 }
 
-getBlogposts(baseURL);
+getPosts(baseURL);
+
+//button actions
+
+prevButton.disabled = true;
+
+prevButton.addEventListener("click", () => {
+    nextButton.disabled = false;
+    const sliderStart = parseInt(
+        getComputedStyle(slider).getPropertyValue("--slider-index")
+    );
+    
+    console.log(sliderStart);
+    if (sliderStart ===1) {
+        prevButton.disabled = true;
+    }
+    slider.style.setProperty("--slider-index", sliderStart - 1);
+});
+
+const screenSize = window.outerWidth;
+
+nextButton.addEventListener("click", () => {
+    prevButton.disabled = false;
+    const sliderStart = parseInt(
+    getComputedStyle(slider).getPropertyValue("--slider-index")
+    );
+
+    if (screenSize <= 699 && sliderStart === 10) {
+        nextButton.disabled = true;
+      } else if (screenSize > 699 && screenSize <= 999 && sliderStart === 4) {
+        nextButton.disabled = true;
+      } else if (screenSize > 999 && screenSize <= 1199 && sliderStart === 2) {
+        nextButton.disabled = true;
+      } else if (screenSize > 1199 && sliderStart === 1) {
+        nextButton.disabled = true;
+      }
+      slider.style.setProperty("--slider-index", sliderStart + 1);
+});
+
